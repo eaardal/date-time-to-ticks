a148a0f99f38856b4005ab1b75681ede0310b2c9
Eirik Årdal <eiard@statoil.com>
2015-10-29 11:06:25 +0100

    Initial commit with completed app

---
+++ DateTimeToTicks/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace DateTimeToTicks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHelper = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(15, 83);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(182, 20);
+            this.txtResult.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Get Ticks";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(15, 28);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(182, 20);
+            this.txtInput.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "DD.MM.YYYY (HH:MM:SS)";
+            // 
+            // txtHelper
+            // 
+            this.txtHelper.AutoSize = true;
+            this.txtHelper.Location = new System.Drawing.Point(12, 106);
+            this.txtHelper.Name = "txtHelper";
+            this.txtHelper.Size = new System.Drawing.Size(0, 13);
+            this.txtHelper.TabIndex = 5;
+            this.txtHelper.Click += new System.EventHandler(this.txtHelper_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(122, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Copy ticks";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(209, 251);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtHelper);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtResult);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ticks";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txtHelper;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ DateTimeToTicks/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace DateTimeToTicks
+{
+    public partial class Form1 : Form
+    {
+        private readonly string[] _formats = new[] { "dd.MM.yyyy HH:mm:ss", "d.M.yyyy HH:mm:ss", "dd.MM.yyyy", "d.M.yyyy" };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                button2.Enabled = false;
+
+                var dateTimeText = txtInput.Text;
+
+                DateTime dateTime = DateTime.MinValue;
+                var wasParsed = false;
+
+                foreach (var format in _formats)
+                {  
+                    wasParsed = DateTime.TryParseExact(dateTimeText, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out dateTime);
+                    if (wasParsed) break;
+                }
+
+                if (wasParsed && dateTime != DateTime.MinValue)
+                {
+                    txtHelper.Text =
+                        string.Format("Day: {0} ({6})\nMonth: {1}\nYear: {2}\nHour: {3}\nMin: {4}\nSec: {5}",
+                            dateTime.Day, dateTime.Month, dateTime.Year, dateTime.Hour, dateTime.Minute, dateTime.Second,
+                            dateTime.DayOfWeek);
+
+                    txtResult.Text = dateTime.Ticks.ToString();
+
+                    button2.Enabled = true;
+                }
+                else
+                {
+                    throw new InvalidOperationException("Could not parse input date and time with the given format");
+                }    
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                button2.Text = "Copy ticks";
+            }
+        }
+
+        private void txtHelper_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtResult.Text);
+            button2.Text = "Copied";
+        }
+    }
+}
